b3d1140eb5d2dceae5922c35ca1c73fbfd156869
Ashish Singh <Type in your email>
2017-12-31 19:37:55 +0530

    Jeson web token configured with authentication user

--- Gemfile
+++ Gemfile
@@ -46,5 +46,6 @@ group :development do
   gem 'spring-watcher-listen', '~> 2.0.0'
 end
 gem 'bcrypt', '~> 3.1', '>= 3.1.11'
+gem 'jwt'
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
--- app/controllers/concerns/exception_handler.rb
+++ app/controllers/concerns/exception_handler.rb
@@ -2,13 +2,42 @@ module ExceptionHandler
   # provides the more graceful `included` method
   extend ActiveSupport::Concern
 
-  included do
-    rescue_from ActiveRecord::RecordNotFound do |e|
-      json_response({message: e.message}, :not_found)
-    end
 
-    rescue_from ActiveRecord::RecordInvalid do |e|
-      json_response({message: e.message}, :unprocessable_entity)
-    end
+
+  class AuthenticationError < StandardError; end
+  class MissingToken < StandardError; end
+  class InvalidToken < StandardError; end
+  class ExpiredSignature < StandardError; end
+
+  included do
+    # Define custom handler
+    rescue_from ActiveRecord::RecordInvalid, with: :four_twenty_two
+    rescue_from ExceptionHandler::AuthenticationError, with: :unauthorized_request
+    rescue_from ExceptionHandler::MissingToken, with: :four_twenty_two
+    rescue_from ExceptionHandler::InvalidToken, with: :four_twenty_two
+    rescue_from ExceptionHandler::ExpiredSignature, with: :four_ninety_eight
+
+    rescue_from ActiveRecord::RecordNotFound do |e|
+    json_response({ message: e.message}, :not_found)
+  end
+ end
+
+  private
+
+  #json response with messaage; Status code 422 - unprocessable entity
+
+  def four_twenty_two(e)
+    json_response({ messaage: e.message }, :unprocessable_entity)
+  end
+  #json response with messaage; Status code 498 - Invalid Token
+
+  def four_ninety_eight(e)
+    json_response({ messaage: e.message }, :invalid_token)
+  end
+
+  #json response with message; status code 401 - Unauthorized
+
+  def unauthorized_request(e)
+    json_response({ message: e.message }, :unauthorized)
   end
 end
